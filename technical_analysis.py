dd1cec90330af8a0d6e5152d10335d1c5c20e91c
jurchiks33 <jurisslisans@gmail.com>
2023-11-10 19:36:35 +0100

    work on technical analysis tools

---
+++ technical_analysis.py
@@ -0,0 +1,9 @@
+import yfinance as yf
+import pandas as pd
+
+def fetch_stock_data(ticker, start_date, end_date):
+    data = yf.download(ticker, start=start_date, end=end_date)
+    return data
+
+def calculate_sma(data, window):
+    return data['Close'].rolling(window=window).mean()
