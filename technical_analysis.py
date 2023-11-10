d66a18c84423b152d9a5cad35a031a857b8a3cc7
jurchiks33 <jurisslisans@gmail.com>
2023-11-10 19:51:53 +0100

    more indicators

--- technical_analysis.py
+++ technical_analysis.py
@@ -1,5 +1,6 @@
 import yfinance as yf
 import pandas as pd
+import numpy as np
 
 def fetch_stock_data(ticker, start_date, end_date):
     data = yf.download(ticker, start=start_date, end=end_date)
@@ -10,3 +11,11 @@ def calculate_sma(data, window):
 
 def calculate_ema(data, window):
     return data['Close'].ewm(span=window, adjust=False).mean()
+
+def calculate_rsi(data, window):
+    delta = data['Close'].diff()
+    gain = (delta.where(delta > 0, 0)).rolling(window=window).mean()
+    loss = (-delta.where(delta < 0, 0)).rolling(window=window).mean()
+    rs = gain / loss
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
